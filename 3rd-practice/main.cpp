5b7d19823012cc5e2a43cd9ee0026bf562139bd3
ming-ki <wbakswl010@naver.com>
2021-09-23 13:02:46 +0900

    commit

---
+++ 3rd-practice/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include "Circle.h"
+
+int main() {
+	Circle donut;
+	double area = donut.getArea();
+	cout << "donut 면적은 " << area << endl;
+
+	Circle pizza(30);
+	area = pizza.getArea();
+	cout << "pizza 면적은 " << area << endl;
+}
